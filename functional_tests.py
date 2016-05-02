8f6989fce3dad52f222b462762d617bd865b97aa
Szymczyk, Grzegorz <grzegorz.szymczyk@wincor-nixdorf.com>
2016-05-02 23:00:54 +0200

    modified test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert "Django" in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title_start(self):
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn("Lists", self.browser.title)
+        self.fail("End of test")
+        
+if __name__=="__main__":
+    unittest.main(warnings='ignore')
+    
